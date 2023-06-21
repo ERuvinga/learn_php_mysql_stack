a0e64beaea6ac9da4bad08a6ca6f1541e910e0da
ERuvinga <ruvingaelie@gmail.com>
2023-06-21 10:44:47 +0200

    fix registerprocessor page

--- pages/registerprocess.php
+++ pages/registerprocess.php
@@ -3,10 +3,12 @@
     $email = $_POST['email'];
     $pwrd = $_POST['pswrd'];
     $name = $_POST['name'];
-    $imgname = "image.jpg"
+    $imgname = "image.jpg";
 
     $sql = 'INSERT INTO user(name, email,pswd, img) VALUES ("'.$name.'","'.$email.'","'.$name.'","'.$imgname.'")';
     $result = $con->query($sql);
 
-    echo($sql);
+    if($result){
+        header("location:usertable.php?msg=compte créer avec succès");
+    }
 ?>
