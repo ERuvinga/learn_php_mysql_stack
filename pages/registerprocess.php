6065c73ec7445eb0d3c54aae32ce15edaa7d2704
ERuvinga <ruvingaelie@gmail.com>
2023-06-21 12:34:09 +0200

    update  in datatbase

--- pages/registerprocess.php
+++ pages/registerprocess.php
@@ -5,10 +5,16 @@
     $name = $_POST['name'];
     $imgname = "image.jpg";
 
-    $sql = 'INSERT INTO user(name, email,pswd, img) VALUES ("'.$name.'","'.$email.'","'.$name.'","'.$imgname.'")';
+    $sql = 'INSERT INTO user(name, email,pswd, img) VALUES ("'.$name.'","'.$email.'","'.$pwrd.'","'.$imgname.'")';
     $result = $con->query($sql);
 
     if($result){
         header("location:usertable.php?msg=compte créer avec succès");
+        exit();
+    }
+
+    else{
+        header("location:register.php?error=compte non crée");
+        exit();
     }
 ?>
